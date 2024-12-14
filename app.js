090767d0361e6759e4c240a6301d58c5d44e35c8
Wilkennn <wilken.henrique2513@gmail.com>
2024-12-14 17:00:36 -0300

    Wilken

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ const app = express();
 
 app.use(cors());
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
 app.use(cors());
 app.use(session({
   secret: process.env.SESSION_SECRET || 'seu-segredo',
